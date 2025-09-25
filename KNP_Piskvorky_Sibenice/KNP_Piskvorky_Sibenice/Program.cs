640a77999855780d04cf2fb23ad72a11c9aead47
Jan Horák <horakj24@spstrutnov.cz>
2025-09-25 08:15:09 +0200

    Upload věcí z PC
    Ještě to musím roztřídit asi

---
+++ KNP_Piskvorky_Sibenice/KNP_Piskvorky_Sibenice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KNP_Piskvorky_Sibenice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
