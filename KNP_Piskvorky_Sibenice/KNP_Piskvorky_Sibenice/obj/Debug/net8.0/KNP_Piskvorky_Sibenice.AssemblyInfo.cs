4b7dc67b88383e0cb059b82b650757d6033ce3f6
Jan Horák <horakj24@spstrutnov.cz>
2025-09-25 08:23:08 +0200

    Propojení s Git asi
    moc tomu nerozumim, snad jsem to nerozbil

--- KNP_Piskvorky_Sibenice/KNP_Piskvorky_Sibenice/obj/Debug/net8.0/KNP_Piskvorky_Sibenice.AssemblyInfo.cs
+++ KNP_Piskvorky_Sibenice/KNP_Piskvorky_Sibenice/obj/Debug/net8.0/KNP_Piskvorky_Sibenice.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KNP_Piskvorky_Sibenice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+640a77999855780d04cf2fb23ad72a11c9aead47")]
 [assembly: System.Reflection.AssemblyProductAttribute("KNP_Piskvorky_Sibenice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KNP_Piskvorky_Sibenice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
